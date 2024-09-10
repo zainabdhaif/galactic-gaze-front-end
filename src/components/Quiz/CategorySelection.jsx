d5cdaa7d4c516516922e9de56f7e5a484339ba59
Nour Bahar <nourbahar@gmail.com>
2024-09-10 10:36:50 +0300

    quiz styling

--- src/components/Quiz/CategorySelection.jsx
+++ src/components/Quiz/CategorySelection.jsx
@@ -9,13 +9,17 @@ const CategorySelection = ({ onSelectCategory }) => {
             <p className='description'>
                 Test your astronomy knowledge with our captivating space-themed quizzes! Whether you're passionate about planets, stars, or galaxies, you'll find a quiz that suits you. Get instant feedback on every question and retake any quiz until you achieve a perfect score. Dive in and challenge yourself to uncover the mysteries of the universe!
             </p>
-            <h5 className='h5'>Select a category to begin </h5>
-            <div className="category-div1">
+           
+            <div className="quiz-card-container">
                 {categories.map((category, index) => (
-                    <button key={index} onClick={() => onSelectCategory(category.name)}>
+                    <div className="quiz-card" key={index}>
+                        <h3>{category.name}</h3>
+                        <p className="quiz-description">{category.description}</p>
                         <img src={category.image} alt={category.name} className="category-image" />
-                        {category.name}
-                    </button>
+                        <button className="select-category-button" onClick={() => onSelectCategory(category.name)}>
+                        Let's dive in
+                        </button>
+                    </div>
                 ))}
             </div>
         </div>
--- src/components/Quiz/categoriesData.js
+++ src/components/Quiz/categoriesData.js
@@ -1,8 +1,23 @@
 const categoriesWithImages = [
-    { name: "Planets", image: "https://cdn.pixabay.com/photo/2012/11/28/10/54/planet-67672_640.jpg" }, 
-    { name: "Stars", image: "https://cdn.pixabay.com/photo/2023/10/10/11/28/star-trails-8306233_640.jpg" },
-    { name: "Galaxies", image: "https://cdn.pixabay.com/photo/2011/12/14/12/17/galaxy-11098_640.jpg" },
-    { name: "Celestial Events", image: "https://cdn.pixabay.com/photo/2016/07/19/04/40/moon-1527501_640.jpg" }, 
+    { 
+      name: "Planets", 
+      image: "https://cdn.pixabay.com/photo/2016/06/24/23/40/space-1478072_1280.jpg",
+      description:"Ready to explore the wildest, most extreme features of the Solar System? From colossal storms to icy volcanoes, every planet, dwarf planet, and moon has its own claim to fame. Join the ultimate Solar System showdown and test your knowledge on these fascinating celestial objects. Let's see how well you know the most mind-blowing facts in space!" 
+    }, 
+    { 
+      name: "Stars", 
+      image: "https://cdn.pixabay.com/photo/2023/10/10/11/28/star-trails-8306233_640.jpg",
+      description:"Stars are the ultimate powerhouses of the universe, each with its own incredible story. From supermassive giants to compact dwarfs, these cosmic titans light up the night sky with astonishing energy. Ready to dive into the dazzling world of stars and see how much you really know? Let’s begin the challenge and uncover the secrets of the stars!" 
+    },
+    { 
+     name: "Galaxies",
+     image: "https://cdn.pixabay.com/photo/2011/12/14/12/17/galaxy-11098_640.jpg",
+     description:"Galaxies are the grand, swirling cities of the universe, each one a collection of billions of stars, planets, and cosmic wonders. From spiral giants to mysterious ellipticals, every galaxy has its own breathtaking tale. Ready to explore the vastness of space and test your galaxy knowledge? Let’s embark on this cosmic journey and see how much you know about these stellar metropolises!"
+     },
+    { name: "Celestial Events",
+      image: "https://cdn.pixabay.com/photo/2016/07/19/04/40/moon-1527501_640.jpg",
+      description:"Celestial events are the spectacular shows of the universe, where the night sky comes alive with incredible phenomena. From eclipses and meteor showers to supernovas and planetary alignments, these events captivate our imagination. Are you ready to dive into the wonders of the cosmos and see how much you know about these awe-inspiring celestial spectacles? Let the quiz begin!"
+     }, 
 ];
 
 export default categoriesWithImages;
