a5fd4294663ddd3681757d337ee5c024baf32a5f
Maryam Qasim <maryam@DESKTOP-6B29GKT.>
2024-09-11 10:58:32 +0300

    mearge work

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -42,7 +42,7 @@ const SignupForm = (props) => {
   };
 
   return (
-    <main className="container mt-5">
+    <main className="container-signin mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
           <h1 className="text-center mb-4">Sign Up</h1>
